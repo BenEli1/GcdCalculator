a1665cc3f3cb16720c923200a183587d711d0faf
Ben Eli <92650578+BenEli1@users.noreply.github.com>
2022-03-17 16:41:42 +0200

    Update gcdCalculator.cpp

--- gcdCalculator.cpp
+++ gcdCalculator.cpp
@@ -4,11 +4,12 @@ using namespace std;
 static long long GCDCalc(long long a,long long b){
     int bigger=myMax(a,b);
     int smaller=myMin(a,b);
-    while(bigger>0 && smaller>0){
-        return GCDCalc(bigger%smaller,smaller);
-    }
-    if(bigger>0)
+    if(bigger==0)
+        return smaller;
+    if(smaller==0)
         return bigger;
-    return smaller;
+    return GCDCalc(bigger%smaller,smaller);
 }
+
+
 
